88182c000f7f6508b55105bf54dda344067f0e25
strahlistvan <strahlistvan0@gmail.com>
2018-02-24 18:52:38 +0100

    Eggsembly decompiler

--- chicken_compiler.js
+++ chicken_compiler.js
@@ -10,38 +10,47 @@ var asmEggsemblyConverter = {	asmEggsemblyMap : { exit: "axe",
 								convertAsm2Egg : function(asm) {
 									if (this.asmEggsemblyMap[asm]) {
 										var str = this.asmEggsemblyMap[asm];
-										return str.split(' ')[0]; 
+										return str.split(' ')[0];  // only instructions, without arguments
 									}
-									return asm;
-									
-								}											
+									return asm;	
+								},
+
+								convertEggsm2Asm : function(eggsm) {
+									for (var prop in this.asmEggsemblyMap) {
+										if (this.asmEggsemblyMap[prop] == eggsm) {
+											return prop;
+										}
+									}
+									return eggsm; // if not found in dictionary
+								}
 							};
-/** Convert chicken source code into Opcode. Return Array of Opcodes. */
-function chicken2Optcode(input) {
+
+/** Compile chicken source code into Opcode. Return Array of Opcodes. */
+function chicken2opcode(input) {
 	var output = [];
-	var input = input.replace(/[\r\n]/g, ' \r\n'); //handle zero chickens
 	var linesArray = input.split(/[\r\n]+/);
 	
-	//optcode = chicken keywords  count in a line
-	output = linesArray.map(str => (str.trim() == "")? 0 : str.split(/\s+/).length);
+	//opcode = chicken keywords  count in a line
+	output = linesArray.map(str => (str.trim() == "")? 0 : str.trim()
+	.split(/\s+/).length);
 	
 	return output;
 }
 
-/** Convert Opcode to ChickenASM code
+/** Compile Opcode to ChickenASM code
  * Input: Array of Opcodes or Opcode string delimeted by new lines
  * Output: Array of ChickenASM instructions
  */
-function optcode2chickenASM(input) {
+function opcode2chickenASM(input) {
 	var output = [];
 	var isNewToken = true;
 	
 	//handle raw string and line splitted input too
-	var optcodeArray = (typeof(input) == "string") ? input.split(/[\r\n]+/) : input;
+	var opcodeArray = (typeof(input) == "string") ? input.split(/[\r\n]+/) : input;
 
-	for (var i=0; i<optcodeArray.length; ++i) {
+	for (var i=0; i<opcodeArray.length; ++i) {
 		 	
-		switch (optcodeArray[i]) {
+		switch (opcodeArray[i]) {
 		case 0:
 			if (isNewToken)
 				asmLine = "exit";
@@ -79,7 +88,7 @@ function optcode2chickenASM(input) {
 			asmLine = "char";
 			break;
 		default: 
-			asmLine = "push "+(parseInt(optcodeArray[i])-10);
+			asmLine = "push "+(parseInt(opcodeArray[i])-10);
 		}
 		
 		//load instruction is double-wide instruction
@@ -93,19 +102,22 @@ function optcode2chickenASM(input) {
 	return output;
 }
 
-/** Convert native Chicken source code to ChickenASM code
+/** Compile Chicken source code to ChickenASM code
  * Input: Chicken source code string
  * Output: Array of ChickenASM instructions
  */
 function chicken2chickenASM(input) {
 	document.getElementById('output').value = "";
 	
-	var optcode = chicken2Optcode(input);
-	var output = optcode2chickenASM(optcode);
+	var opcode = chicken2opcode(input);
+	var output = opcode2chickenASM(opcode);
 	
 	for (var i=0; i<output.length; ++i) {
 		document.getElementById('output').value += output[i] + '\n';	
 	}
+	
+	console.log("chickenAsm2chicken");
+	console.log(chickenAsm2chicken(output));
 	
 	return output;
 }
@@ -114,28 +126,111 @@ function chicken2chickenASM(input) {
  * Input: Array of ChickenASM instructions or ChickenASM source code string
  * Output: Array of Eggsembly instructions
  */
-function chickenASM2Eggsembly(input) {
+function chickenAsm2Eggsembly(input) {
 	var output = [];
 
 	//handle raw string and line splitted input too
 	var linesArray = (typeof(input) == "string") ? input.split(/[\r\n]+/) : input;
 	
 	for (var i=0; i<linesArray.length; ++i) {
-		output.push(asmEggsemblyConverter.convertAsm2Egg(linesArray[i]));
+		var eggsm = asmEggsemblyConverter.convertAsm2Egg(linesArray[i])
+		output.push(eggsm);		
 	}
 
 	return output;
 }
 
-/** Convert native Chicken source code to Eggsembly code
+/** Convert Eggsembly code to ChickenASM code
+ * Input: Array of Eggsembly instructions or Eggsembly source code string
+ * Output: Array of ChickenASM instructions
+ */
+function eggsembly2ChickenAsm(input) {
+	var output = [];
+
+	//handle raw string and line splitted input too
+	var linesArray = (typeof(input) == "string") ? input.split(/[\r\n]+/) : input;
+	
+	for (var i=0; i<linesArray.length; ++i) {
+		var asm = asmEggsemblyConverter.convertEggsm2Asm(linesArray[i])
+		output.push(asm);		
+	}
+
+	return output;
+}
+
+/** Compile Chicken source code to Eggsembly code
  * Input: Chicken source code string
  * Output: Array of Eggsembly instructions
  */
 function chicken2Eggsembly(input) {
 	var chickenAsmArray = chicken2chickenASM(input);
-	var eggsemblyArray = chickenASM2Eggsembly(chickenAsmArray);
+	var eggsemblyArray = chickenAsm2Eggsembly(chickenAsmArray);
 	
 	for (var i=0; i<eggsemblyArray.length; ++i) {
 		document.getElementById("output_egg").innerHTML += eggsemblyArray[i] + "\n";
 	}
+	
+	console.log(" eggsembly2chicken: ");
+	console.log(eggsembly2chicken(eggsemblyArray));
+	
+	return eggsemblyArray;
+}
+
+/** Decompile opcode array to chicken source string */
+function opcode2chicken(input) {
+	var output = "";
+	for (var i=0; i<input.length; ++i) {
+		var output = output + Array(input[i]+1).join(" chicken") + "\n";
+	}
+	
+	return output;
+}
+
+/** Decompile ChickenASM code to optocodes.
+	Input: Array of ChickenASM instructions or ChickenASM source code string.
+	Output: Array of Opcodes
+*/
+function chickenAsm2opcode(input) {
+	
+	var output = [];
+	//handle raw string and line splitted input too
+	var linesArray = (typeof(input) == "string") ? input.split(/[\r\n]+/) : input;
+	
+	var instructions = ["exit", "chicken", "add", "substract", "multiply", "compare", "load", "store", "jump", "char"];
+	
+	for (var i=0; i<linesArray.length; ++i) {
+		var line = linesArray[i];
+
+		var instr = line.split(' ')[0]; 
+		if (instr !== "push") {
+			output.push(instructions.indexOf(instr));
+			if (instr == "load") { //load is double-wide instruction
+				var loadParam = line.split(' ')[1];
+				output.push(loadParam);
+			}
+		}
+		else { //push
+			var pushParam = line.split(' ')[1];
+			output.push(pushParam+10);
+		}
+	}
+	
+	return output;
+}
+
+/** Decompile ChickenASM to Chicken source code */
+function chickenAsm2chicken(input) {	
+	var opcodeArray = chickenAsm2opcode(input);
+	var chickenCode = opcode2chicken(opcodeArray);
+	
+	return chickenCode;
+}
+
+/** Decompile Eggsembly code to Chicken source code */
+function eggsembly2chicken(input) {
+	var asmArray = eggsembly2ChickenAsm(input);
+	var opcodeArray = chickenAsm2opcode(asmArray);
+	var chickenCode = opcode2chicken(opcodeArray);
+	
+	return chickenCode;
 }
